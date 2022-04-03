4e55e6c2821367a81ffceab32703817f95ff325f
TerryLai422 <35938314+TerryLai422@users.noreply.github.com>
2022-04-03 16:12:04 -0400

    add endpoint 2 configuration files

---
+++ jwt-endpoint-2/src/main/java/com/thinkbox/jwt/JWTEndpoint2Application.java
@@ -0,0 +1,15 @@
+package com.thinkbox.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+//@EnableEurekaClient
+@SpringBootApplication
+public class JWTEndpoint2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JWTEndpoint2Application.class, args);
+	}
+
+}
